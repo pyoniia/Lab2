38f5cc33f684c3dd54884a458317da901596ed78
pyoniia <83009214+pyoniia@users.noreply.github.com>
2021-04-29 20:18:47 +0200

    Update makeChocolate.java
    Try modulo %=

--- makeChocolate.java
+++ makeChocolate.java
@@ -2,7 +2,7 @@ public int makeChocolate(int small, int big, int goal) {
     big = big*5; //one big equals 5 smalls;
   int remain = 0;
   if (goal<= big + small){  
-    return remain = goal-big; //if it is possible to make chocolate returns remain
+    return remain = goal-big; //if it is possible to make chocolate returns remain 
   }else{
     return -1;
   }
