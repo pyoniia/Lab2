5f89743dc59fb9d707a219928571b678b66bac83
pyoniia <83009214+pyoniia@users.noreply.github.com>
2021-04-29 20:21:27 +0200

    Update makeChocolate.java

--- makeChocolate.java
+++ makeChocolate.java
@@ -2,7 +2,8 @@ public int makeChocolate(int small, int big, int goal) {
     big = big*5; //one big equals 5 smalls;
   int remain = 0;
   if (goal<= big + small){  
-    return remain = goal-big; //if it is possible to make chocolate returns remain 
+    remain = goal%=big; // modulo, goal divided by big gives remaining
+    return remain = remain; 
   }else{
     return -1;
   }
